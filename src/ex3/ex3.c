bd9257d77ce28b5f86027f27c8c57d22518145be
theluismen <luismaocampo28@gmail.com>
2025-12-24 16:25:53 +0100

    feat: ex3 hecho

--- src/ex3/ex3.c
+++ src/ex3/ex3.c
@@ -6,45 +6,48 @@
 
 #define QUEUE_NAME		"/cola"
 #define QUEUE_MAX_MSG 	10
-#define QUEUE_MSG_SIZE	32
+#define QUEUE_MSG_SIZE	8
 
 mqd_t cola;
 
 void * funcion_hilo_prod ( void * arg ) {
-	char mensaje[QUEUE_MSG_SIZE];
-	unsigned int prioridad = 1;
+	char mensaje[QUEUE_MSG_SIZE+1];
+	unsigned int prioridad, i;
 	
-	
-    for ( int i = 0; i < 5; i++ ) {
-		sprintf(mensaje, "Hola como tas %d", i);
+    for ( i = 0; i < QUEUE_MAX_MSG; i++ ) {
+		sprintf(mensaje, "MSG %d", i);
+		prioridad = i % 2;
 		if ( mq_send(cola, mensaje, strlen(mensaje)+1, prioridad) != -1 ) {
-			printf("[PROD] - Enviado Mensaje: %d %s", i, mensaje);
+			printf("[PROD] -> (%8s,%2d)\n", mensaje, prioridad);
 		} else {
-			perror("mq_send");
+			perror("mq_send error");
 		}	
 	}
+	
+	printf("\n");
+	
+	return NULL;
 }
 
 void * funcion_hilo_cons ( void * arg ) {
-    char mensaje[QUEUE_MSG_SIZE];
-    unsigned int prioridad;
-    ssize_t bytes_recibidos;
+    char mensaje[QUEUE_MSG_SIZE+1];
+    unsigned int prioridad, i;
+    ssize_t msg_len;
 
-    while (1) {
+    for ( i = 0; i < QUEUE_MAX_MSG; i++ ) {
         // Bloqueante: espera hasta que haya un mensaje
-        bytes_recibidos = mq_receive(cola, mensaje, QUEUE_MSG_SIZE, &prioridad);
+        msg_len = mq_receive(cola, mensaje, QUEUE_MSG_SIZE, &prioridad);
 
-        if (bytes_recibidos >= 0) {
+        if (msg_len >= 0) {
             // Asegurarse de que el mensaje está null-terminated
-            mensaje[bytes_recibidos] = '\0';
-            printf("[CONS] - Recibido Mensaje (prio %u): %s\n", prioridad, mensaje);
+            mensaje[msg_len] = '\0';
+            printf("[CONS] <- (%8s,%2d)\n", mensaje, prioridad);
         } else {
             perror("mq_receive");
         }
-
-        // Simular tiempo de procesamiento
-        //usleep(150000); // 150 ms
     }
+
+	printf("\n");
 
     return NULL;
 }
@@ -59,17 +62,18 @@ int main ( void ) {
 	attr.mq_msgsize = QUEUE_MSG_SIZE;
 	attr.mq_curmsgs = 0;
 	
+	mq_unlink(QUEUE_NAME);
 	cola = mq_open(QUEUE_NAME, O_CREAT | O_RDWR, 0660, &attr);
 
 	if ( cola == (mqd_t)-1 ) {
-		printf("ERROR ABRIR COLA\n");
+		perror("ERROR ABRIR COLA\n");
 		exit(1);
 	}
 
     pthread_create(&hilo_prod, NULL, funcion_hilo_prod, NULL);
-    pthread_create(&hilo_cons, NULL, funcion_hilo_cons, NULL);
-
     pthread_join(hilo_prod, NULL);
+    
+    pthread_create(&hilo_cons, NULL, funcion_hilo_cons, NULL);
     pthread_join(hilo_cons, NULL);
     
     mq_close(cola);
