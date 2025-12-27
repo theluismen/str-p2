f5d78a041a692c2977c27fab27855f8cb5c4da36
theluismen <luismaocampo28@gmail.com>
2025-12-27 17:38:05 +0100

    ex1 modificado

--- src/ex1/ex1.c
+++ src/ex1/ex1.c
@@ -1,89 +1,34 @@
-//#include <stdio.h>
-//#include <stdlib.h>
-//#include <pthread.h>
-
-//void * funcion_hilo ( void * arg ) {
-    //int id = *(int *)arg;
-
-    //for ( int i = 0; i < 5; i++ ) {
-        //printf("Hilo %d\n", id);
-    //}
-//}
-
-//int main ( int argc, char * argv[] ) {
-    //pthread_t * hilos;
-    //int * ids;
-    //pthread_attr_t attr_fifo, attr_rr;
-    //struct sched_param sp_fifo, sp_rr;
-    //int num_hilos;
-
-    ///* Comprobar argumentos */
-    //if ( argc != 2 ) {
-        //return 1;
-    //}
-
-    //num_hilos 	= atoi(argv[1]);
-    //hilos 		= calloc(num_hilos, sizeof(pthread_t));
-	//ids 		= calloc(num_hilos, sizeof(int));
-	
-    //pthread_attr_init(&attr_fifo);
-    //pthread_attr_setschedpolicy(&attr_fifo, SCHED_FIFO);
-    //sp_fifo.sched_priority = 10;
-    //pthread_attr_setschedparam(&attr_fifo, &sp_fifo);
-    
-    //pthread_attr_init(&attr_rr);
-    //pthread_attr_setschedpolicy(&attr_rr, SCHED_FIFO);
-	//sp_rr.sched_priority = 20;
-    //pthread_attr_setschedparam(&attr_rr, &sp_rr);
-    
-    //printf("Num Hilos: %d\n\n", num_hilos);
-
-    //for ( int i = 0; i < num_hilos; i++ ) {
-		//ids[i] = i;
-        //pthread_create(&hilos[i], ( i & 1 == 0 ) ? &attr_fifo : &attr_rr, funcion_hilo, &ids[i]);
-    //}
-
-    //for ( int i = 0; i < num_hilos; i++ ) {
-        //pthread_join(hilos[i], NULL);
-    //}
-
-    //return 0;
-//}
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sched.h>
 
-// Número de hilos
 #define NUM_THREADS 3
 
 // Estructura para los hilos
-typedef struct {
-    int thread_id;
-    int policy;
-    int priority;
-} thread_data_t;
+struct Hilo_Info {
+    int hilo;
+    int sched;
+    int prior;
+};
 
 // Función que será ejecutada por cada hilo
-void* thread_function(void* arg) {
-    thread_data_t* data = (thread_data_t*) arg;
+void * funcion_hilo ( void * arg ) {
+    struct Hilo_Info * data = ( struct Hilo_Info * ) arg;
     struct sched_param param;
+    int i;
     
-    // Establecer la prioridad para cada hilo
-    param.sched_priority = data->priority;
+    param.sched_priority = data->prior;
     
-    if (pthread_setschedparam(pthread_self(), data->policy, &param) != 0) {
-        perror("Error al establecer la política de programación");
-        pthread_exit(NULL);
-    }
+    pthread_setschedparam(pthread_self(), data->sched, &param);
     
-    printf("Hilo %d: Política = %d, Prioridad = %d\n", data->thread_id, data->policy, data->priority);
+    printf("Hilo %d: SCHED = %d, PRIOR = %d\n", data->hilo, data->sched, data->prior);
     
     // Hacer que el hilo realice alguna operación para observar el comportamiento
-    for (int i = 0; i < 5; i++) {
-        printf("Hilo %d en ejecución... Iteración %d\n", data->thread_id, i);
-        //usleep(500000); // 0.5 segundos
+    for ( i = 0; i < 10; i++ ) {
+        printf("Hilo %d : %d\n", data->hilo, i);
+        //usleep(500000);
 		for (int i = 0; i < 100000000; i++) {
 			// Realizar un cálculo sencillo para consumir tiempo de CPU.
 			volatile int temp = i * i;
@@ -94,28 +39,25 @@ void* thread_function(void* arg) {
 }
 
 int main() {
-    pthread_t threads[NUM_THREADS];
-    thread_data_t thread_data[NUM_THREADS];
+    pthread_t hilos[NUM_THREADS];
+    struct Hilo_Info hilos_info[NUM_THREADS];
+    int i;
     
     // Establecer políticas y prioridades para los hilos
-    int policies[] = {SCHED_FIFO, SCHED_FIFO, SCHED_FIFO };  // Tres políticas de planificación
-    int priorities[] = {30, 20, 10};  // Diferentes prioridades
+    int scheds[] = {SCHED_FIFO, SCHED_FIFO, SCHED_FIFO };  // Tres políticas de planificación
+    int priors[] = {30, 30, 30};  // Diferentes prioridades
     
-    for (int i = 0; i < NUM_THREADS; i++) {
-        thread_data[i].thread_id = i + 1;
-        thread_data[i].policy = policies[i];
-        thread_data[i].priority = priorities[i];
+    for ( i = 0; i < NUM_THREADS; i++ ) {
+        hilos_info[i].hilo = i + 1;
+        hilos_info[i].sched = scheds[i];
+        hilos_info[i].prior = priors[i];
         
-        // Crear los hilos
-        if (pthread_create(&threads[i], NULL, thread_function, (void*) &thread_data[i]) != 0) {
-            perror("Error al crear el hilo");
-            exit(EXIT_FAILURE);
-        }
+        pthread_create(&hilos[i], NULL, funcion_hilo, (void*) &hilos_info[i]);
     }
     
     // Esperar a que todos los hilos terminen
-    for (int i = 0; i < NUM_THREADS; i++) {
-        pthread_join(threads[i], NULL);
+    for ( i = 0; i < NUM_THREADS; i++ ) {
+        pthread_join(hilos[i], NULL);
     }
     
     return 0;
